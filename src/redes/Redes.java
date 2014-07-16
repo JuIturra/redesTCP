1f7ff809a4dbc7c5da2688744898d42a52c9cefd
JuIturra <ian.zephir@gmail.com>
2014-07-16 07:13:03 -0400

    Se han arreglado algunas cosas
    Ahora se puede hacer la peticion http y recibir datos, pero hay un reset
    de conexion

--- src/redes/Redes.java
+++ src/redes/Redes.java
@@ -5,8 +5,6 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Redes {
 
@@ -14,55 +12,39 @@ public class Redes {
         //se crean los socket
         ServerSocket servidor = null;
         Socket sock = null;
+        byte[] datos = new byte[2048];
+        DataInputStream input = null;
+        DataOutputStream output = null;
+        DataOutputStream peticion = null;
+        
         try{
-            servidor = new ServerSocket(8500);
+            servidor = new ServerSocket(8400); //socket del servidor
+            sock = new Socket("127.0.0.1",8500); //socket del cliente
         }catch(Exception e){
             e.printStackTrace();
         }
         
-        Socket clientSock = null;
-        String peticion_HTTP = "GET / HTTP/1.1\r\nHost: "+"192.168.1.2"+":"+"8400"+"\r\nConnection: keep-alive\r\n\r\n";
+        Socket clientSock = servidor.accept(); //socket de conexion para el servidor
+        String peticion_HTTP = "GET / HTTP/1.1\r\nHost: "+"127.0.0.1"+":"+"8500"+"\r\nConnection: keep-alive\r\n\r\n";
+        //peticion = new DataOutputStream(sock.getOutputStream());
+        //peticion.writeBytes(peticion_HTTP);
+        //peticion.flush();
+        if(clientSock.isConnected()) System.out.println("esta conectado");
         
-        while(true){
-            try{
-                sock = new Socket("192.168.1.2",8080);
-                clientSock = servidor.accept();
-                final Socket fin = clientSock;
-                Thread trd = new Thread(new Runnable(){
-                    @Override
-                    public void run(){
-                        try{
-                            Thread.sleep(5000);
-                        }catch(Exception e){
-                            e.printStackTrace();
-                        }
-                        
-                        DataInputStream input = null;
-                        DataOutputStream output = null;
-                        
-                        try {
-                            output = new DataOutputStream(fin.getOutputStream());
-                        } catch (IOException ex) {
-                            Logger.getLogger(Redes.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                        try {
-                            output.writeBytes(peticion_HTTP);
-                        } catch (IOException ex) {
-                            Logger.getLogger(Redes.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                        try {
-                            output.flush();
-                        } catch (IOException ex) {
-                            Logger.getLogger(Redes.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                        //entrada
-                        
-                    }
-                });
-            }catch(Exception e){
-                e.printStackTrace();
-            }
+        Boolean run = true;
+        while(run){
+            System.out.println("Enviando peticion");
+            peticion = new DataOutputStream(sock.getOutputStream());
+            peticion.writeBytes(peticion_HTTP);
+            peticion.flush();
+            System.out.println("Trafico de datos");
+            input = new DataInputStream(sock.getInputStream());
+            int tamaño = input.read(datos,0,datos.length);
+            output = new DataOutputStream(clientSock.getOutputStream());
+            output.write(datos,0,tamaño);
         }
-        
+        sock.close();
+        clientSock.close();
+        servidor.close();
     } 
 } 
