a0ba4c3e0931d67398260b0ff0bd32d14b769f40
JuIturra <ian.zephir@gmail.com>
2014-07-21 20:39:43 -0400

    Version final con TCP
    Ahora funciona la restransmicion de datos mediante protocolo http con el
    programa VLC

--- src/redes/Redes.java
+++ src/redes/Redes.java
@@ -18,30 +18,28 @@ public class Redes {
         DataOutputStream peticion = null;
         
         try{
-            servidor = new ServerSocket(8400); //socket del servidor
-            sock = new Socket("127.0.0.1",8500); //socket del cliente
+            servidor = new ServerSocket(8400); //socket receptor en puerto 8400
+            sock = new Socket("ip_origen_video",8500); //socket del origen en puerto 8500
         }catch(Exception e){
             e.printStackTrace();
         }
         
         Socket clientSock = servidor.accept(); //socket de conexion para el servidor
-        String peticion_HTTP = "GET / HTTP/1.1\r\nHost: "+"127.0.0.1"+":"+"8500"+"\r\nConnection: keep-alive\r\n\r\n";
-        //peticion = new DataOutputStream(sock.getOutputStream());
-        //peticion.writeBytes(peticion_HTTP);
-        //peticion.flush();
-        if(clientSock.isConnected()) System.out.println("esta conectado");
+        String peticion_HTTP = "GET / HTTP/1.1\r\nHost: "+"ip_origen_video"+":"+"Puerto"+"\r\nConnection: keep-alive\r\n\r\n";
+        peticion = new DataOutputStream(sock.getOutputStream());
+        peticion.writeBytes(peticion_HTTP);
+        peticion.flush();
         
-        Boolean run = true;
+        System.out.println("Enviando peticion");
+        if(clientSock.isConnected()) System.out.println("esta conectado");
+        input = new DataInputStream(sock.getInputStream());
+        output = new DataOutputStream(clientSock.getOutputStream());
+        boolean run = true;
         while(run){
-            System.out.println("Enviando peticion");
-            peticion = new DataOutputStream(sock.getOutputStream());
-            peticion.writeBytes(peticion_HTTP);
-            peticion.flush();
-            System.out.println("Trafico de datos");
-            input = new DataInputStream(sock.getInputStream());
-            int tamaño = input.read(datos,0,datos.length);
-            output = new DataOutputStream(clientSock.getOutputStream());
+            System.out.println("Datos recibidos");
+            int tamaño = input.read(datos,0,datos.length);            
             output.write(datos,0,tamaño);
+            output.flush();
         }
         sock.close();
         clientSock.close();
